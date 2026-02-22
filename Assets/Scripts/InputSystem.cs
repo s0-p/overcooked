3906775a2ea2e295fdd4abd50a3f2b0b02bc493f
s0-p <b00v0429@gmail.com>
2026-02-22 13:31:47 +0900

    add dash function

--- Assets/Scripts/InputSystem.cs
+++ Assets/Scripts/InputSystem.cs
@@ -100,6 +100,15 @@ public partial class @InputSystem: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": true
+                },
+                {
+                    ""name"": ""Dash"",
+                    ""type"": ""Button"",
+                    ""id"": ""20c2a9ca-cc00-48ac-9f0c-86c23964b685"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -157,6 +166,17 @@ public partial class @InputSystem: IInputActionCollection2, IDisposable
                     ""action"": ""Move"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""bed63dd7-f40c-4a6c-9005-fc0a4be48f00"",
+                    ""path"": ""<Keyboard>/shift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Dash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -183,6 +203,7 @@ public partial class @InputSystem: IInputActionCollection2, IDisposable
         // Player
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+        m_Player_Dash = m_Player.FindAction("Dash", throwIfNotFound: true);
     }
 
     ~@InputSystem()
@@ -264,6 +285,7 @@ public partial class @InputSystem: IInputActionCollection2, IDisposable
     private readonly InputActionMap m_Player;
     private List<IPlayerActions> m_PlayerActionsCallbackInterfaces = new List<IPlayerActions>();
     private readonly InputAction m_Player_Move;
+    private readonly InputAction m_Player_Dash;
     /// <summary>
     /// Provides access to input actions defined in input action map "Player".
     /// </summary>
@@ -279,6 +301,10 @@ public partial class @InputSystem: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "Player/Move".
         /// </summary>
         public InputAction @Move => m_Wrapper.m_Player_Move;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Dash".
+        /// </summary>
+        public InputAction @Dash => m_Wrapper.m_Player_Dash;
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -308,6 +334,9 @@ public partial class @InputSystem: IInputActionCollection2, IDisposable
             @Move.started += instance.OnMove;
             @Move.performed += instance.OnMove;
             @Move.canceled += instance.OnMove;
+            @Dash.started += instance.OnDash;
+            @Dash.performed += instance.OnDash;
+            @Dash.canceled += instance.OnDash;
         }
 
         /// <summary>
@@ -322,6 +351,9 @@ public partial class @InputSystem: IInputActionCollection2, IDisposable
             @Move.started -= instance.OnMove;
             @Move.performed -= instance.OnMove;
             @Move.canceled -= instance.OnMove;
+            @Dash.started -= instance.OnDash;
+            @Dash.performed -= instance.OnDash;
+            @Dash.canceled -= instance.OnDash;
         }
 
         /// <summary>
@@ -382,5 +414,12 @@ public partial class @InputSystem: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnMove(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Dash" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnDash(InputAction.CallbackContext context);
     }
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,14 +1,20 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
     CharacterController _charCtrl;
 
-    [SerializeField]
+    //[SerializeField]
     float _moveSpeed = 6f;
     Vector3 _moveDir = default;
-    [SerializeField]
+
+    //[SerializeField]
+    float _dashSpeed = 7f;
+    //[SerializeField]
+    float _dashDuration = 0.2f;
+    float _dashTimer = 0f;
+
+    //[SerializeField]
     float _rotateSpeed = 30f;
 
     InputSystem.PlayerActions _playerInput;
@@ -18,8 +24,11 @@ public class PlayerMove : MonoBehaviour
         _charCtrl = GetComponent<CharacterController>();
 
         _playerInput = InputManager.Instance.Player;
+        
         _playerInput.Move.performed += OnMove;
         _playerInput.Move.canceled += OnMove;
+
+        _playerInput.Dash.performed += OnDash;
     }
     private void Update()
     {
@@ -27,13 +36,31 @@ public class PlayerMove : MonoBehaviour
     }
     private void Move()
     {
-        _charCtrl.Move(Time.deltaTime * Mathf.Min(_moveDir.magnitude, 1f) * _moveSpeed * _moveDir.normalized);
-        transform.forward = Vector3.Lerp(transform.forward, _moveDir, Time.deltaTime * _rotateSpeed);
+        Vector3 dir = Vector3.zero;
+        float velocity = 0f;
+
+        dir = _moveDir;
+        velocity = Time.deltaTime * Mathf.Min(dir.magnitude, 1f) * _moveSpeed;
+
+        if (_dashTimer > 0f)
+        {
+            _dashTimer -= Time.deltaTime;
+            dir = transform.forward;
+            velocity = Time.deltaTime * _dashSpeed;
+        }
+
+        transform.forward = Vector3.Slerp(transform.forward, dir, Time.deltaTime * _rotateSpeed);
+        _charCtrl.Move(velocity * dir.normalized);
     }
     public void OnMove(UnityEngine.InputSystem.InputAction.CallbackContext ctx)
     {
         Vector2 inputVal = ctx.ReadValue<Vector2>();
         _moveDir.x = inputVal.x;
         _moveDir.z = inputVal.y;
+    }
+    public void OnDash(UnityEngine.InputSystem.InputAction.CallbackContext ctx)
+    {
+        if (ctx.ReadValueAsButton() && _dashTimer <= 0f)
+            _dashTimer = _dashDuration;
     }
 }
